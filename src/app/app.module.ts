5b01e9de2d111647906325a573c33e1f82f4fdcd
Haohan Yang <yanghaohanmail@gmail.com>
2023-08-18 22:03:52 +0200

    add basic pwa features #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core"
+import { NgModule, isDevMode } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { ReactiveFormsModule } from "@angular/forms"
 import { AppRoutingModule } from "./app-routing.module"
@@ -17,7 +17,8 @@ import { AboutComponent } from "./pages/about/about.component"
 import { SaveListModalComponent } from "./components/save-list-modal/save-list-modal.component"
 import { DeleteModalComponent } from "./components/delete-modal/delete-modal.component"
 import { SearchWordModalComponent } from "./components/search-word-modal/search-word-modal.component"
-import { SearchResultEntryComponent } from "./components/search-result-entry/search-result-entry.component"
+import { SearchResultEntryComponent } from "./components/search-result-entry/search-result-entry.component";
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 @NgModule({
   declarations: [
@@ -39,7 +40,13 @@ import { SearchResultEntryComponent } from "./components/search-result-entry/sea
     AmplifyAuthenticatorModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [HttpService, AuthService],
   bootstrap: [AppComponent]
